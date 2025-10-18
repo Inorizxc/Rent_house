e63c4cae95e21cdcf31b493e6bd3c5ef0bec1f47
Inori <Inori936@yandex.ru>
2025-10-18 19:19:05 +0400

    For Kirill

--- database/migrations/2025_10_18_125823_create_roles_table.php
+++ database/migrations/2025_10_18_125823_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table -> id();
-            $table->stirng("UniqName") ->unique();
+            $table->string("UniqName") ->unique();
             $table->string("Name");
             $table->text("Description");
             $table->timestamps();
